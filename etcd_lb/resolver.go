c49c6288ab6da8c3830b20ba13ba931120be5e66
BuTou1030 <1549773802@qq.com>
2022-07-09 23:21:10 +0800

    init commit

---
+++ etcd_lb/resolver.go
@@ -0,0 +1,38 @@
+package etcd_lb
+
+import (
+	"fmt"
+
+	"google.golang.org/grpc/naming"
+)
+
+type resolver struct {
+	projectName string
+	moduleName  string
+	targetAddr  string
+	userName    string
+	passWord    string
+}
+
+// projectName：项目名称，用于树形存储的根路径
+// moduleName：模块名称(当前服务名称)，同一项目下包含多个模块，用于树形存储。projectName + moduleName下存储同一模块的一个或多个地址
+// targetAddr：目标注册中心地址，如etcd数据库、Redis数据库等
+// userName：注册中心登录用户名
+// passWord：注册中心登录密码
+func NewResolver(projectName, moduleName, userName, passWord string) *resolver {
+	return &resolver{
+		projectName: projectName,
+		moduleName:  moduleName,
+		userName:    userName,
+		passWord:    passWord,
+	}
+}
+
+func (re *resolver) Resolve(target string) (naming.Watcher, error) {
+	if re.projectName == "" || re.moduleName == "" {
+		return nil, fmt.Errorf("projectName or moduleName was nil")
+	}
+	re.targetAddr = target
+
+	return &watcher{resolver: re, isInitialized: false}, nil
+}
---
+++ etcd_lb/server_register.go
@@ -0,0 +1,42 @@
+package etcd_lb
+
+import (
+	"fmt"
+	"log"
+	"skywatcher/etcd_lb/etcd"
+	"strings"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+)
+
+// Register 非阻塞，在主线程中调用该方法一次，以心跳形式将当前服务地址写入服务注册中心。
+// projectName：项目名称，用于树形存储的根路径
+// moduleName：模块名称(当前服务名称)，同一项目下包含多个模块，用于树形存储。projectName + moduleName下存储同一模块的一个或多个地址
+// addr：当前被注册的服务地址，一般为"ip:port"等
+// targetAddr：目标注册中心地址，如etcd数据库、Redis数据库等
+// userName：注册中心登录用户名
+// passWord：注册中心登录密码
+// interval：注册的心跳周期(单位：s)，每间隔该周期会重新注册一次
+// lifeCycle：注册信息的生命周期，超过lifeCycle个心跳周期(即lifeCycle * interval)信息即失效
+func Register(projectName, moduleName, addr, targetAddr, userName, passWord string, interval, lyfeCycle uint64) error {
+	var key = fmt.Sprintf("/%s/%s/%s", projectName, moduleName, addr)
+
+	go func() {
+		ticker := time.NewTicker(time.Duration(interval) * time.Second)
+		for {
+			// 获取LeaseID
+			if leaseID, err := etcd.GetGrant(strings.Split(targetAddr, ","), userName, passWord, int64(interval*lyfeCycle)); err != nil {
+				log.Printf("Grant failed, %+v\n", err)
+			} else {
+				if err := etcd.PutKeyValue(strings.Split(targetAddr, ","), userName, passWord, key, addr, clientv3.WithLease(leaseID)); err != nil {
+					log.Printf("Put failed, %+v\n", err)
+				}
+			}
+
+			<-ticker.C
+		}
+	}()
+
+	return nil
+}
